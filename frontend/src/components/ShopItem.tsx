60215000ab1834e2271a8bc5fde2ac7b6f4cdd1c
Elor-Itz <elor2200@gmail.com>
2025-04-27 17:56:46 +0300

    Added icon

--- frontend/src/components/ShopItem.tsx
+++ frontend/src/components/ShopItem.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import { Item } from "../services/item-service";
 import "../styles/ShopItem.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 interface ShopItemProps {
   item: Item;
@@ -11,11 +13,15 @@ interface ShopItemProps {
 const ShopItem: React.FC<ShopItemProps> = ({ item, onAddToCart }) => {
   return (
     <div className="shop-item">
-      <img src={item.imageURL || "/images/placeholder_image.png"} alt={item.name} />
+      <img
+        src={item.imageURL || "/images/placeholder_image.png"}
+        alt={item.name}
+      />
       <h3>{item.name}</h3>
       <p>Category: {item.category}</p>
       <p>Price: ${item.price.toFixed(2)}</p>
       <Button variant="primary" onClick={() => onAddToCart(item)}>
+        <FontAwesomeIcon icon={faShoppingCart} className="me-2" />
         Add to Cart
       </Button>
     </div>
